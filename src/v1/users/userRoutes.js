aafd830c491e03d6c3bbed7d5463a8da2f2e5f83
TCaken <cakentan@gmail.com>
2023-09-21 21:16:25 +0800

    With user profile pages

--- src/v1/users/userRoutes.js
+++ src/v1/users/userRoutes.js
@@ -8,11 +8,14 @@ const router = express.Router();
 
 router.get("/", userService.getAllUsers);
 
-router.post("/", userService.createUser)
+router.post("/", userService.createUser);
+
+//router.get("/profile/:userId", userService.get)
+//router.post("/topup/:userId", userService.topup)
 
 router.get("/:userId", userService.getUser);
 
 router.delete("/:userId", userService.deleteUser)
-  
+
 
 module.exports = router;
